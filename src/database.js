7538c5a32e08e67836dcdab9d6dcefaf1198d2a9
LucasCorbalan <lucas.corbalan.23@gmail.com>
2023-11-25 14:30:34 -0300

    add some change in mongosse

--- src/database.js
+++ src/database.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 import 'dotenv/config'
 
-//const url = "mongodb://127.0.0.1:27017/GymnasioBD";
+const url = "mongodb://127.0.0.1:27017/GymnasioBD";
 
-const URI = process.env.MONGODB_URL
-console.log(URI);
-//mongoose.connect(url);
-mongoose.connect(URI);
-
+//const URI = process.env.MONGODB_URL
 //console.log(URI);
+
+
+mongoose.connect(url);
+//mongoose.connect(URI);
+
 const connetion = mongoose.connection;
 
 connetion.once("open", () => {
