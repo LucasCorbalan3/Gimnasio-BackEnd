029a74e1882a6bf88fd50030e7b0b85491a33f24
LucasCorbalan <lucas.corbalan.23@gmail.com>
2023-12-06 11:14:49 -0300

    shift to local development mode

--- src/database.js
+++ src/database.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
-import 'dotenv/config'
+import "dotenv/config";
 
-// const url = "mongodb://127.0.0.1:27017/GymnasioBD";
-const URI = process.env.MONGODB_URL
-console.log(URI);
+const url = "mongodb://127.0.0.1:27017/GymnasioBD";
+// const URI = process.env.MONGODB_URL
+// console.log(URI);
 
-//mongoose.connect(url);
-mongoose.connect(URI);
+mongoose.connect(url);
+// mongoose.connect(URI);
 
 //console.log(URI);
 const connetion = mongoose.connection;
@@ -14,4 +14,3 @@ const connetion = mongoose.connection;
 connetion.once("open", () => {
   console.log("BD Conectada");
 });
-
